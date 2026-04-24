edd872d64018fa356132573f89c6476aa05490dd
xAndrija <aandrijastojanovicc23@gmail.com>
2026-04-24 11:39:02 +0100

    api fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,10 +4,10 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import { useEffect, useState } from "react";
 import NewsList from "../../components/NewsList";
-import { getNews } from "../../../api/newsApi";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContextValue";
 import { useCallback } from "react";
+import { getNews } from "../../api/newsApi";
 
 function Home() {
 
